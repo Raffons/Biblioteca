2d033a8ca757af8a3690eae9fe6e7a47cdbfe6e6
Simone Raffoni <SIMONE.RAFFONI@local.ittcesena.it>
2025-11-21 08:42:18 +0100

    Realizzati costruttore e metoto ToString() della classe Libro

--- Biblioteca/Biblioteca/Libro.cs
+++ Biblioteca/Biblioteca/Libro.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Biblioteca
 {
-    internal class Libro
+    internal class Libro                        // Classe Libro
     {
+        public string Titolo { get; set; }                                            
+        public string Autore { get; set; }
+        public string Editore { get; set; }
+        public int AnnoDiPubblicazione{ get; set; }
+        public int NumeroPagine { get; set; }
+
+        public Libro(string titolo, int numeroPagine, string autore, int annoDiPubblicazione, string editore)  // Costruttore con tutti i parametri 
+        {
+            Titolo = titolo;
+            Autore = autore;
+            AnnoDiPubblicazione = annoDiPubblicazione;
+            Editore = editore;
+            NumeroPagine = numeroPagine;
+        }
+        public override string ToString()
+        {
+            return $"{Titolo} by {Autore}, Published in {AnnoDiPubblicazione}, {Editore}, {NumeroPagine}";
+        }
     }
 }
