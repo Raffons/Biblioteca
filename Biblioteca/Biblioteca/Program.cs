e02ef0ab1aa0518d2ce76263f489573ea43df98d
Simone Raffoni <SIMONE.RAFFONI@local.ittcesena.it>
2025-11-21 09:31:13 +0100

    Versione non finita della classe program()

--- Biblioteca/Biblioteca/Program.cs
+++ Biblioteca/Biblioteca/Program.cs
@@ -1,13 +1,15 @@
-﻿namespace Biblioteca
+﻿using System.Runtime.InteropServices;
+
+namespace Biblioteca
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-          Libro libro1 = new Libro();
+          Libro libro1 = new Libro();   // Creazione del primo libro
           Biblioteca biblioteca = new Biblioteca ();
-          Libro libro2 = new Libro(); 
-          
+          Libro libro2 = new Libro();  // Creazione del secondo libro
+
 
         }
     }
