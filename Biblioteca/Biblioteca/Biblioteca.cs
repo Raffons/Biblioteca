f38f6d908da0b8036a6039773e1d8cda62a8653d
Simone Raffoni <SIMONE.RAFFONI@local.ittcesena.it>
2025-11-21 09:13:51 +0100

    Inizializzata classe Biblioteca e scrittura della lista e del costruttore

--- Biblioteca/Biblioteca/Biblioteca.cs
+++ Biblioteca/Biblioteca/Biblioteca.cs
@@ -8,5 +8,20 @@ namespace Biblioteca
 {
     internal class Biblioteca
     {
+        public string Nome { get; set; }   
+        public string Indirizzo { get; set; }
+        public int OrariApertura { get; set; }
+        public int OrariChiusura { get; set; }
+        public List<Libro> Libri { get; set; } // Lista che contiene i libri della biblioteca 
+        public Biblioteca(string nome, string indirizzo, int orariApertura, int orariChiusura) // Costruttore con i parametri nome, indirizzo, orari apertura e chiusura
+        {
+            Nome = nome;
+            Indirizzo = indirizzo;
+            OrariApertura = orariApertura;
+            OrariChiusura = orariChiusura;
+            Libri = new List<Libro>();
+        }
+
+       
     }
 }
